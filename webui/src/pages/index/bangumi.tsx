5326baf53257a6a0c4323b33fcee0f2fff900bf9
KotaHv <92137267+KotaHv@users.noreply.github.com>
2026-09-04 05:46:47 +0800

    fix(webui): adjust bangumi grid layout

--- webui/src/pages/index/bangumi.tsx
+++ webui/src/pages/index/bangumi.tsx
@@ -15,7 +15,7 @@ export default function BangumiPage() {
 
   return (
     <div className="h-full grow overflow-auto p-6">
-      <div className="grid grid-cols-2 gap-5 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8">
+      <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
         {bangumi?.map((i) => (
           <div key={i.id} className={i.deleted ? 'grayscale' : ''}>
             <AbBangumiCard
